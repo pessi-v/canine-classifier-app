247cb0cca98a1449960916924c30b24c4a29e9b2
pessi-v <pessimo@posteo.net>
2024-11-18 15:12:48 +0100

    update streamlit to 1.40.1, edit layout

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## Canine Classifier App
 
+A Streamlit app for the frontend of https://github.com/pessi-v/canine-classifier-app. Identify dog breeds using an ML model. Built in LeWagon Data Science and AI bootcamp.
+
 Run Streamlit web server:
+
 ```bash
 make streamlit
 ```
--- upload.py
+++ upload.py
@@ -1,26 +1,34 @@
 import streamlit as st
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 6MB
 
 st.set_page_config(
-    page_title="Canine Classifier 🐶",
+    page_title="Canine Classifier",
     page_icon="🐶",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="collapsed"
 )
 with open("styles/main.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-MAX_FILE_SIZE = 6 * 1024 * 1024  # 6MB
-
-col1, col2 = st.columns(2, gap="medium")
-
-col1.title("Canine Classifier 🐶")
-col1.subheader("Identify your dogs breed!")
 
 for key in st.session_state.keys():
     del st.session_state[key]
 
-upload = col1.file_uploader("Upload an image of your dog", type=["png", "jpg", "jpeg"])
-cam = col1.camera_input("Take a picture of your dog", label_visibility="visible")
-col2.image("images/orange_doggo.png", use_column_width=True)
+
+
+column_1, column_2 = st.columns(2, gap="medium", vertical_alignment="bottom")
+
+# column 1
+with column_1:
+
+    st.title("Canine Classifier")
+    st.subheader("Identify a dog's breed!")
+    upload = st.file_uploader("Upload an image of a dog", label_visibility='hidden', type=["png", "jpg", "jpeg"], accept_multiple_files=False)
+    enable = st.checkbox("Enable webcam input")
+    camera = st.camera_input("Take a picture of a dog with your webcam", label_visibility="visible", disabled=not enable)
+
+# column 2
+with column_2:
+    st.image("images/orange_doggo.png", use_container_width=True)
 
 if upload is not None:
     if upload.size > MAX_FILE_SIZE:
@@ -30,9 +38,9 @@ if upload is not None:
 
         st.switch_page("pages/crop.py")
 
-if cam is not None:
-    if cam.size > MAX_FILE_SIZE:
+if camera is not None:
+    if camera.size > MAX_FILE_SIZE:
         st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
     else:
-        st.session_state.upload = cam.read()
+        st.session_state.upload = camera.read()
         st.switch_page("pages/crop.py")
