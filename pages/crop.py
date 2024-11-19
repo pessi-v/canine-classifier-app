2270c86ec4c66881945bc01c7b5614f99a4ea722
pessi-v <pessimo@posteo.net>
2024-11-19 16:12:00 +0100

    add st_cropper, without removing the old one yet

--- pages/crop.py
+++ pages/crop.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from streamlit_cropperjs import st_cropperjs
+from streamlit_cropper import st_cropper
+from PIL import Image
+import io
 
 st.set_page_config(
     page_title="Canine Classifier",
@@ -23,9 +26,26 @@ def select_borders(uploaded_file):
         st.session_state.cropped_pic = cropped_pic
         st.switch_page("pages/results.py")
 
+def select_borders_2(uploaded_file):
+    # breakpoint()
+    # img = Image.open(uploaded_file)
+    img = Image.open(io.BytesIO(uploaded_file))
+    cropped_img = st_cropper(
+        img,
+        realtime_update=True,
+        aspect_ratio=None)
+
+    st.write("Preview")
+    _ = cropped_img.thumbnail((150,150))
+    st.image(cropped_img)
+    st.session_state.cropped_pic = cropped_img
+    if st.button("Identify", use_container_width=True):
+        st.switch_page("pages/results.py")
+    # st.page_link("pages/results.py", label="Identify")
 
 try:
-    select_borders(st.session_state.uploaded_file)
+    # select_borders(st.session_state.uploaded_file)
+    select_borders_2(st.session_state.uploaded_file)
 except Exception as e:
     print(e)
     st.switch_page("app.py")
